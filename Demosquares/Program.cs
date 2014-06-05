45c364ba4ace951cdbb3011dd8bd86d124337da0
smoore8 <savionmoore8@gmail.com>
2014-06-05 10:06:11 -0400

    Not Finished

---
+++ Demosquares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoSquares
+{
+    class Square
+    {
+        static void Main(string[] args)
+        {
+            Square[] oneside = new Square[10];
+
+
+            for (int i = 0; i < oneside.Length; i++)
+            {
+                oneside[i] = new Square(i + 10);
+                Console.WriteLine(oneside[i].Area);
+            }
+        }
+    }
+}
